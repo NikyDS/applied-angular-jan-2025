1adacd835837aadd20dbb5189a810c17b60d3e32
NikyDS <dossantosnikoly01@gmail.com>
2025-01-28 12:34:28 -0500

    before activity

--- src/app/demos/components/basic-signals.component.ts
+++ src/app/demos/components/basic-signals.component.ts
@@ -4,60 +4,29 @@ import {
   signal,
   computed,
 } from '@angular/core';
+import { GolfScoreComponent } from './golf-score.component';
+import { AnimalListComponent } from './animal-list.component';
+import { AddAnimalComponent } from './add-animal.component';
 
 @Component({
   selector: 'app-basic-signals',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [GolfScoreComponent, AnimalListComponent, AddAnimalComponent],
   template: `
-    <p>
-      Your Current Score:
-      <span>{{ currentScore() }}</span>
-    </p>
-    <p>
-      Your Current Score:
-      <span>{{ currentScore() }}</span>
-    </p>
-    <p></p>
-
     <div>
-      <button (click)="addStroke()" class="btn btn-primary">Took A Shot</button>
-    </div>
+      <h2 class="text-2xl">Golfing!</h2>
+      <app-golf-score />
 
-    @for (animal of animals(); track $index) {
-      <div>{{ animal }}</div>
-    }
-
-    <div>
-      <p>Warning - bad code for a11y. don't judge. we'll cover that later.</p>
-
-      <input
-        #newAnimalInput
-        type="text"
-        class="input input-sm input-bordered"
-      />
-      <button
-        (click)="addAnimal(newAnimalInput.value)"
-        class="btn btn-secondary"
-      >
-        Add Animal
-      </button>
-      <p>You currently have {{ animalCount() }} animals!</p>
+      <app-animal-list [list]="animals()" />
+      <app-add-animal (animalAdded)="addAnimal($event)" />
     </div>
   `,
   styles: ``,
 })
 export class BasicSignalsComponent {
-  currentScore = signal(0); // state
-  animals = signal(['dog', 'cat', 'mouse']);
-  animalCount = computed(() => this.animals().length);
-
+  animals = signal(['Bird', 'Goat', 'Deer']);
+  // animalCount = computed(() => this.animals().length);
   addAnimal(animalName: string) {
     this.animals.update((currentAnimals) => [animalName, ...currentAnimals]);
-  }
-  addStroke() {
-    // this.currentScore.set(this.currentScore() + 1); // changeable
-    this.currentScore.update((c) => c + 1);
-    // hey, update the dom wherever the currentScore is shown now.
   }
 }
